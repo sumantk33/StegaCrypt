52045f249c39debee0e41d37d1b1af499b53efb2
Sameer Katti <sameerkatti@Sameers-MacBook-Pro.local>
2021-06-09 19:45:33 +0530

    Time taken feature added

---
+++ code/static/scripts/index.js
@@ -0,0 +1,7 @@
+document.getElementById("button1").onclick = function () {
+    location.href = "/encryption";
+};
+
+document.getElementById("button2").onclick = function () {
+    location.href = "/decryption";
+};
